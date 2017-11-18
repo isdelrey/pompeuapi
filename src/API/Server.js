df7872c059fbecdba80b6cab19345a98d08abedb
Ivø <ivosequeros@me.com>
2017-11-18 01:21:43 +0100

    Set up Babel to Deploy to Flynn 🌈
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/API/Server.js

--- src/API/Server.js
+++ src/API/Server.js
@@ -3,17 +3,13 @@ import Routes from './Routes'
 import fs from 'fs'
 export default class {
     constructor() {
-        let https_options = {
-            key: fs.readFileSync('/etc/letsencrypt/live/vultr.ivosequeros.com/privkey.pem'),
-            certificate: fs.readFileSync('/etc/letsencrypt/live/vultr.ivosequeros.com/fullchain.pem')
-        };
-        this.server = Restify.createServer(https_options)
+        this.server = Restify.createServer()
         this.server.use(Restify.plugins.queryParser())
         this.server.use(Restify.plugins.bodyParser())
         Routes(this.server)
     }
     start() {
-        this.server.listen(443, () => {
+        this.server.listen(80, () => {
             console.log('%s listening at %s', this.server.name, this.server.url)
           });
     }
